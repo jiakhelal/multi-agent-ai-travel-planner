a86348e9b46d9f192886530807c1e654146102a1
jiakhelal <138600169+jiakhelal@users.noreply.github.com>
2026-06-13 17:56:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import operator
 from typing import TypedDict, Annotated
+import streamlit as st
 
 from dotenv import load_dotenv
 
@@ -27,7 +28,8 @@ print("WEATHER KEY =", os.getenv("OPENWEATHER_API_KEY"))
 
 # LLM
 llm = ChatGroq(
-    model="llama-3.3-70b-versatile"
+    model="llama-3.3-70b-versatile",
+    api_key=st.secrets["GROQ_API_KEY"]
 )
 
 # State
